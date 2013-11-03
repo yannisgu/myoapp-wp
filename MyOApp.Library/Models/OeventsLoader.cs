f66ced6bff86d9469b7eec9c5ed2d02adcf89193
Yannis Güdel <me@yannisgudel.ch>
2013-11-03 01:56:44 +0100

    added missing files

---
+++ MyOApp.Library/Models/OeventsLoader.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOApp.Library.Models
+{
+    public class OeventsLoader
+    {
+        string BASE_URL = "http://oevents.aws.af.cm/";
+
+        public async Task<List<Event>> GetEvents()
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, BASE_URL + "events?source=solv");
+            var response = await httpClient.SendAsync(request);
+
+            var events = JsonConvert.DeserializeObject<Event[]>( await response.Content.ReadAsStringAsync());
+            return events.ToList();
+        }
+
+    }
+}
---
+++ MyOApp.Library/ViewModels/EventItemViewModel.cs
@@ -0,0 +1,64 @@
+﻿using MyOApp.Library.Models;
+using System;
+
+
+namespace MyOApp.Library.ViewModels
+{
+    public class EventItemViewModel : PropertyChangedBase
+    {
+        public EventItemViewModel(Event selectedEvent)
+        {
+            LoadDataModel(selectedEvent);
+        }
+
+        public void LoadDataModel(Event model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            Date = model.Date;
+            Map= model.Map;
+            Organiser = model.Organiser;
+            Region = model.Region;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public string MapDate
+        {
+            get
+            {
+                string value = "";
+                if(!string.IsNullOrEmpty(Map))
+                {
+                    value += Map += ", ";
+                }
+                value += Date.ToString("d");
+                return value;
+            }
+        }
+
+        public string RegionOrganiser
+        {
+            get
+            {
+                string value = "";
+
+                value += Region;
+                if(!string.IsNullOrEmpty(Region) && !string.IsNullOrEmpty(Organiser))
+                {
+                    value += ", ";
+                }
+                value += Organiser;
+                return value;
+            }
+        }
+
+        public string Map { get; set; }
+
+        public string Region { get; set; }
+
+        public string Organiser { get; set; }
+    }
+}
